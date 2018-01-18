313ad60796563b172545858bf6b24604514a7411
fantomFire <yilong8903@163.com>
2018-01-18 15:56:52 +0800

    chen

--- app/src/main/java/com/zhss/zhss_sjlm/MainActivity.java
+++ app/src/main/java/com/zhss/zhss_sjlm/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("6256565464658");
+        System.out.println("6256565464658");
     }
 }
